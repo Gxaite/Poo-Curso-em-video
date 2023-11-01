7000b225af2ef3f2ac1a2c2371b3b4961f916788
Gabriel Scheidt <gabrielscheidt2000@gmail.com>
2023-11-01 15:36:18 -0300

    Aula 6 Poo

---
+++ src/aula6/program.java
@@ -0,0 +1,22 @@
+package aula6;
+
+public class program {
+
+	public static void main(String[] args) {
+		ControleRemoto cr1 = new ControleRemoto();
+		cr1.abriMenu();
+		cr1.ligar();
+		cr1.maisVolume();
+		cr1.maisVolume();
+		cr1.ligarMudo();
+		cr1.abriMenu();
+		
+		
+		
+		
+		cr1.abriMenu();
+		
+
+	}
+
+}
